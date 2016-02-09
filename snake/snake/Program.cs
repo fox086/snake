52ebe561197b0e0cd0393cee6e871820be145220
fox086 <foxy086@mail.ru>
2016-02-09 22:20:38 +0300

    VerticalLine

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -16,8 +16,11 @@ namespace snake
             Point p2 = new Point(4,5,'#');
             p2.Draw();
 
-            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
-            line.Drow();
+            HorizontalLine hLine = new HorizontalLine(5, 10, 8, '+');
+            hLine.Drow();
+
+            VerticalLine vLine = new VerticalLine(10, 15, 12, '%');
+            vLine.Drow();
            
             Console.ReadLine();
          }
